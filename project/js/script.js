582671fb069564e6a3162016885a1b9b21d2166d
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2025-01-31 14:31:02 +0300

    toggleHeart

--- project/js/script.js
+++ project/js/script.js
@@ -50,6 +50,12 @@ const model = {
         this.notes.unshift(newNote); 
         view.renderNotes(this.notes);
     },
+    toggleHeart(id) {
+      const note = this.notes.find(note => note.id === id);
+      note.isFavorite = !note.isFavorite
+      
+      view.renderNotes(this.notes);
+    }
     
     
     };
@@ -62,24 +68,33 @@ const view = {
     const inputDescription = document.querySelector("#note-description");
     const choiseColor = document.querySelector(".radio-list");
     let color = document.querySelector('input[name="color"]:checked').value;
+    const list = document.querySelector(".notes-list");
     
-    choiseColor.addEventListener("change", function (event) {
-      if (event.target.name === "color") {
-        color = event.target.value;
-      }
+      choiseColor.addEventListener("change", function (event) {
+        if (event.target.name === "color") {
+          color = event.target.value;
+        };
 
-      form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        const title = inputTitle.value;
-        const description = inputDescription.value;
+        form.addEventListener("submit", function (event) {
+          event.preventDefault();
+          const title = inputTitle.value;
+          const description = inputDescription.value;
 
-        controller.addNote(title, description, color);
-        inputTitle.value = "";
-        inputDescription.value = "";
+          controller.addNote(title, description, color);
+          inputTitle.value = "";
+          inputDescription.value = "";
 
-        
+          
+        });
       });
-    });
+      list.addEventListener('click', function(event) {
+        if (event.target.classList.contains('heart-inactive') || event.target.classList.contains('heart-active')) {
+          const id = +event.target.closest('.note-frame').id
+          controller.toggleHeart(id);
+        }
+      })
+
+
   },
   
   renderNotes(notes) {
@@ -92,13 +107,12 @@ const view = {
     for (let i = 0; i < notes.length; i++) {
       const note = notes[i];
         notesHTML += `
-              <li class="note-frame ${note.color} ${
-          note.isFavorite ? "favorite" : ""
-        }" id="${note.id}">
+              <li class="note-frame ${note.color} ${note.isFavorite ? "favorite" : ""}" id="${note.id}">
                   <div class="note-frame-header">
                       <h2 class="note-title">${note.title}</h2>
                       <div class="note-icons">
-                          <img class="heart-inactive" src="assets/heart-inactive.png" alt="INACTIVE">
+                          <img class="${note.isFavorite ? "heart-active" : "heart-inactive"}" 
+                          src="${note.isFavorite ? "assets/heart-active.png" : "assets/heart-inactive.png"}" alt="favorite">
                           <img class="trash" src="assets/trash.png" alt="DELETE">
                       </div>
                   </div>
@@ -120,6 +134,9 @@ const controller = {
             //else {} в дальнейшем добавим сообщения об ошибках!
         }
     },
+    toggleHeart(id){
+      model.toggleHeart(id);
+    }
     
 };
 
